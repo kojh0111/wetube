20e5dbc68eb00e58ef8e5006a1958dcc307f6cf6
kojh0111 <kojh0111@naver.com>
2020-11-19 21:24:14 +0900

    Home controller

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
