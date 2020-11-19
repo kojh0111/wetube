5b54a757980d928a29147b250813c73228b922c9
kojh0111 <kojh0111@naver.com>
2020-11-19 22:22:30 +0900

    Join, Login Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,24 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("login", { pageTitle: "Log in" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log in" });
+export const postLogin = (req, res) => res.redirect(routes.home);
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Log out" });
 export const userDetail = (req, res) =>
