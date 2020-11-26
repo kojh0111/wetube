0f035de7bedf59d715e40372e6e66e6ab688e4a5
kojh0111 <kojh0111@naver.com>
2020-11-26 23:19:13 +0900

    Getting Video by ID

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -44,8 +44,12 @@ export const videoDetail = async (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = await Video.findById(id);
-  res.render("videoDetail", { pageTitle: "Video Detail", video });
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
 };
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
