4c367e17976513ef319163fd5af64a044b384742
kojh0111 <kojh0111@naver.com>
2020-12-07 22:31:10 +0900

    User Join and Login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,9 @@
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 },
   } = req;
@@ -16,17 +17,20 @@ export const postJoin = async (req, res) => {
         email,
       });
       await User.register(user, password);
+      next();
     } catch (error) {
       console.log(error);
+      res.render("join", { pageTitle: "Join" });
     }
-    // To Do: Log user in
-    res.redirect(routes.home);
   }
 };
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Log in" });
-export const postLogin = (req, res) => res.redirect(routes.home);
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home,
+});
 
 export const logout = (req, res) => {
   // To Do: Process Log Out
