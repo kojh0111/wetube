b7a617972417331ee755383725127445fb7357f9
kojh0111 <kojh0111@naver.com>
2020-12-22 16:40:39 +0900

    Custom Video Player Part Three: Time

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -61,11 +63,39 @@ function goFullScrn() {
   fullScrnBtn.addEventListener("click", exitFullScrn);
 }
 
+function formatDate(totalSeconds) {
+  const secondsNumber = parseInt(totalSeconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let seconds = secondsNumber - hours * 3600 - minutes * 60;
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function setCurrentTime() {
+  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+}
+
+function setTotalTime() {
+  const totalTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerHTML = totalTimeString;
+}
+
 function init() {
   videoPlayer.addEventListener("click", handlePlayClick);
   document.addEventListener("keydown", keyboardShortcuts);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScrnBtn.addEventListener("click", goFullScrn);
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+  videoPlayer.addEventListener("timeupdate", setCurrentTime);
 }
 
 if (videoContainer) {
